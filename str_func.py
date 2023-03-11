f9fe3bb16bfac33a1e96556ed46e39c4082029b4
yuliazhukova <yuliazhukova@hotmail.com>
2023-03-11 13:40:08 +0100

    add title function and docstrings to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 def capitalize():
+
+    '''Функция, которая делает заглавными все буквы каждого слова в строке,
+     поступившей на вход функции'''
+
     cap_string = str.upper(input())
     return cap_string
+
+def title_format():
+
+    '''Функция, которая делает заглавными первые буквы каждого слова в строке,
+     поступившей на вход функции'''
+
+    title_string = str.title(input())
+    return title_string
